c34cc37900406cf15d609af504a337a3f59ac177
Kelvin Ferreira <kelvin.fds22@gmail.com>
2021-07-06 20:21:38 -0300

    Criação de Hooks das salas e componentes das perguntas. Também foi criada a sala de visualização da parte do Admin

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewRoom } from './Pages/NewRoom';
 import { Room } from './Pages/Room'
 
 import { AuthContextProvider } from './contexts/AuthContexts'
+import { AdminRoom } from './Pages/AdminRoom/AdminRoom';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
